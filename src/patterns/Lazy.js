7ceeb160faea85a9785750155249b9da847a587f
Ashwani <ashwani90125@gmail.com>
2025-02-17 14:58:31 +0530

    Work on create lazy component

---
+++ src/patterns/Lazy.js
@@ -0,0 +1,15 @@
+import React, { Suspense } from 'react';
+
+const LazyComponent = React.lazy(() => import('./LazyComponent'));
+
+function App() {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyComponent />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;

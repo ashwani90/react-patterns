a06b9ba6acb24fc6dfa5330e08a177bd34b0838c
Ashwani <ashwani90125@gmail.com>
2025-02-18 00:40:17 +0530

    Work on render props

---
+++ src/patterns/RenderProps.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+const MouseTracker = ({render}) => {
+    const [position, setPosition] = useState({ x:0, y: 0});
+
+    const handleMouseMove = (event) => {
+        setPosition({ x: event.clientX, y: event.clientY });
+    }
+
+    return (
+        <div style={{ height: '100vh' }} onMouseMove={handleMouseMove}>
+            {render(position)}
+        </div>
+    );
+
+};
+
+
+function App() {
+    return (
+        <MouseTracker render={({x,y}) => (
+            <h1>
+                The mouse position is ({x}, {y})
+            </h1>
+        )} />
+    );
+}
+
+export default App;
